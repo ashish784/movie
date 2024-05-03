af5cd22ee263cd860cbb5df263b783b73a70ead7
ashish784 <mauryaashish784@gmail.com>
2024-05-03 09:10:30 +0530

    last one

--- client/src/components/common/HeroSlide.jsx
+++ client/src/components/common/HeroSlide.jsx
@@ -76,7 +76,7 @@ const HeroSlide = ({ mediaType, mediaCategory }) => {
         loop={movies.length > 1}
         style={{ width: "100%", height: "max-content" }}
       autoplay={{
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false
       }}
       >
